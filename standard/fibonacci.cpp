48710c12ec283261b74da2887af10aab324f9166
Ajay Nair <prodigyaj@gmail.com>
2014-11-27 19:54:29 -0600

    Fibonacci using dynamic programming. Max N = 92

---
+++ standard/fibonacci.cpp
@@ -0,0 +1,40 @@
+/*
+* author: Ajay Nair
+* Fibonacci - using dynamic programming, n<93
+*
+*/
+
+
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+long long memo[95];
+long long solve(int n)
+{
+
+	long long &ret = memo[n];
+	if(ret!=-1)return ret;
+	if(n<=0 || n==1)return n;
+	ret = solve(n-1)+solve(n-2);
+	return ret;
+	
+}
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	memset(memo,-1,sizeof(memo));
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		cout<<solve(n-1)+solve(n-2)<<endl;
+	}
+	return 0;
+}
