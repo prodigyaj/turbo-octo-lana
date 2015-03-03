9e5e26ea49c304966c9610b3e27946267cafb6b0
Ajay Nair <prodigyaj@gmail.com>
2015-03-02 19:10:36 -0600

    O(n) using stack method - Accepted

--- leetcode/largest_rectangle_hist.cpp
+++ leetcode/largest_rectangle_hist.cpp
@@ -9,20 +9,37 @@ public:
     int largestRectangleArea(vector<int> &h) {
 
     	int max_h = 0;
+    	stack <int> st;
     	// Note this is a brute force solution (On^2) and will fail in leetcode
-    	for(int i=0;i<h.size();i++)
+    	int i;
+    	for(i=0;i<h.size();i++)
     	{
-    		int cur = h[i];
-    		max_h = max(max_h,h[i]);
-    		int counter = 1;
-    		for(int j=i+1;j<h.size();j++)
-    		{
-    			counter++;
-    			if(h[j]<cur)
-    				cur = h[j];
-    			max_h = max(max_h,cur*counter); //keep counting the area for every new element we find.
-    		}
-    	}
+    		
+
+    		if(st.empty()==true)
+    			st.push(i);
+    		else
+    			if (h[st.top()] <= h[i])
+    				st.push(i);
+			else
+			{
+				int top = st.top();st.pop();
+				int next_index = (st.empty()==true)?i:i-st.top()-1; // -1 for 0 based,
+				// if stack is empty after pop means that, the current value can be multiplied with all of them seen in the index so far
+				int area = h[top]*next_index;
+				max_h = max(area,max_h);
+				i--; //dont add the current value yet
+			}  
+		}
+
+		while(st.empty()!=true)
+		{
+				int top = st.top();st.pop();
+				int next_index = (st.empty()==true)?h.size():h.size()-st.top()-1; // -1 for 0 based,
+				// if stack is empty after pop means that, the current value can be multiplied with all of them seen in the index so far
+				int area = h[top]*next_index;
+				max_h = max(area,max_h);
+		}  	
         return max_h;
     }
 };
@@ -30,6 +47,7 @@ public:
 int main()
 {
 	int vv[] = {2,1,5,6,2,3};
+	//int vv[] = {2,1,2};
 	vector <int> in (begin(vv),end(vv));
 	Solution s;
 	int val = s.largestRectangleArea(in);
