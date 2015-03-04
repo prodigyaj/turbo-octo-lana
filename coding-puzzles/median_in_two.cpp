bc6a253f9bddf241bfbfe3fba896035e7cb1abc4
Ajay Nair <prodigyaj@gmail.com>
2015-03-04 17:55:37 -0600

    Median Element in two equally sorted arrays

--- coding-puzzles/median_in_two.cpp
+++ coding-puzzles/median_in_two.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 
 using namespace std;
-int t;
+
 int getMedian(int a1[],int l1,int h1,int a2[],int l2,int h2)
 {
 	int m1 = (l1+h1)/2;
 	int m2 = (l2+h2)/2;
 
-
-	cout<<"m1: "<<m1<<" a1: "<<a1[m1]<<" m2: "<<m2<<" a2: "<<a2[m2]<<endl;
-	cin >> t;
 	if(a1[m1] == a2[m2])return a1[m1];
 
-	if(l1 == m1 && l2 == m2)
+	if(l1 == m1 && l2 == m2) // Since m and n equal, total number of elementes is even, so median can be two answers.
 		return max(a1[l1],a2[l2]);
 
 	if(a1[m1] > a2[m2])
 		return getMedian(a1,l1,m1,a2,m2,h2);
-	//if(a1[m1] < a2[m2])
 	else
 		return getMedian(a1,m1,h1,a2,l2,m2);
 }
 
 int main()
 {
-	//int arr1[15],arr2[15];
-
-	int m,n;
+	int m,n; // m and n have to be equal for this two work
 	cin >> m >> n;
 	cout<<"Enter elements for Array 1:\n";
 	for(int i=0;i<m;i++)
@@ -41,6 +35,4 @@ int main()
 	cout<<"Median: "<<val<<endl;
 
 	return 0;
-
-
 }
